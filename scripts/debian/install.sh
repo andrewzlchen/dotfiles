b2421cc9e21bd567adf5429c3cd0d642a92dff0d
Andrew Chen <andrew.chen923@gmail.com>
2017-10-11 19:59:12 +0000

    made a mac folder

--- scripts/debian/install.sh
+++ scripts/debian/install.sh
@@ -6,3 +6,4 @@ apt-get install tmux
 apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln ~/dotfiles/.vimrc ~/.vimrc
