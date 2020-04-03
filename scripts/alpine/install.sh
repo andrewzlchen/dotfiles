c2a2e188dc334eeb333131ffe9b6a08e4fd018c0
Andrew Chen <andrew.chen923@gmail.com>
2020-04-02 23:54:48 -0400

    removed unused scripts

--- scripts/alpine/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apk update
-apk add vim zsh tmux curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
