70cb2c8e4783c0ff36609539b03529e233ae34ad
Andrew Chen <andrew.chen923@gmail.com>
2021-10-10 00:19:48 -0400

    Add buffer navigation maps

--- nvim/lua/core/maps.lua
+++ nvim/lua/core/maps.lua
@@ -9,8 +9,10 @@ map('n', '<C-k>', '<cmd>resize -2<CR>', keymap_opts)
 map('n', '<C-l>', '<C-w>>', keymap_opts)
 
 -- Buffers
-map('n', '<leader>bd', '<cmd>bd<CR>', keymap_opts)
+map('n', '<leader>bn', '<cmd>bn<CR>', keymap_opts)
+map('n', '<leader>bp', '<cmd>bp<CR>', keymap_opts)
 map('n', '<leader>bb', '<cmd>Telescope buffers<CR>', keymap_opts)
+map('n', '<leader>bd', '<cmd>bd<CR>', keymap_opts)
 
 -- Tabs
 map('n', '<leader>tn', '<cmd>tabnext<CR>', keymap_opts)
