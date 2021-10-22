966d3232f9a4585298dde940187a0193fa2069a7
Andrew Chen <andrew.chen@10gen.com>
2021-10-21 23:15:46 -0400

    Update vimtest strategy

--- nvim/lua/plugins/vimtest.lua
+++ nvim/lua/plugins/vimtest.lua
@@ -1,5 +1,7 @@
 local fn = vim.fn
 
+vim.g['test#strategy'] = 'neovim'
+
 if fn.getcwd() == "/Users/andrew.chen/Code/mongodb/baas" then
     vim.g['test#go#gotest#options'] = '-exec="env LD_LIBRARY_PATH=/Users/andrew.chen/Code/mongodb/baas/etc/dylib/lib" -v -tags debug'
 end
