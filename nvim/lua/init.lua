ff35bea044347e2fc6baf265d2096c291493de73
Andrew Chen <andrew.chen@mongodb.com>
2022-12-29 21:42:02 -0800

    restart nvim config

--- nvim/lua/init.lua
+++
@@ -1,3 +0,0 @@
-require('core.options')
-require('core.maps')
-require('plugins.init')
--- nvim/lua/plugins/symbolsOutline.lua
+++
@@ -1,3 +0,0 @@
-local keymap_opts = { noremap=true, silent=true }
-
-vim.api.nvim_set_keymap('n', '<localleader>so', '<cmd>SymbolsOutline<CR>', keymap_opts)
