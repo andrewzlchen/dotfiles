bbd558f5c6508390de5befdc3a132aac47cc94a7
Andrew Chen <andrew.chen@mongodb.com>
2024-11-21 17:53:33 +0000

    use harpoon window instead of telescope

--- nvim/lua/andrewzlchen/plugins/harpoon.lua
+++ nvim/lua/andrewzlchen/plugins/harpoon.lua
@@ -7,33 +7,12 @@ return {
 
     -- REQUIRED
     harpoon:setup()
-    -- REQUIRED
-
-    -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require("telescope.pickers")
-        .new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-        })
-        :find()
-    end
-
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end, { desc = "Add file to harpoon list" })
+
     vim.keymap.set("n", "<leader>ss", function()
-      toggle_telescope(harpoon:list())
+      harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Open harpoon window" })
 
     vim.keymap.set("n", "<leader>1", function()
