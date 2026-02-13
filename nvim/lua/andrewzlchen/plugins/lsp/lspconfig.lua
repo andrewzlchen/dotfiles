3cb1b4d27303d222f5cc50bd5c658f3478411d45
Andrew Chen <andrew.chen@mongodb.com>
2026-02-13 20:48:43 +0000

    update lspconfig

--- nvim/lua/andrewzlchen/plugins/lsp/lspconfig.lua
+++ nvim/lua/andrewzlchen/plugins/lsp/lspconfig.lua
@@ -7,8 +7,13 @@ return {
     { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    -- import mason_lspconfig plugin
     local mason_lspconfig = require("mason-lspconfig")
+    -- Automatically install lsp servers
+    mason_lspconfig.setup({
+      ensure_installed = { "lua_ls" },
+    })
+    -- Automatically enable installed lsp servers
+    mason_lspconfig.setup()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
@@ -17,38 +22,25 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    -- mason_lspconfig.setup_handlers({
-    --   -- default handler for installed servers
-    --   function(server_name)
-    --     lspconfig[server_name].setup({
-    --       capabilities = capabilities,
-    --     })
-    --   end,
-    --   ["lua_ls"] = function()
-    --     -- configure lua server (with special settings)
-    --     lspconfig["lua_ls"].setup({
-    --       capabilities = capabilities,
-    --       settings = {
-    --         Lua = {
-    --           -- make the language server recognize "vim" global
-    --           diagnostics = {
-    --             globals = { "vim" },
-    --           },
-    --           completion = {
-    --             callSnippet = "Replace",
-    --           },
-    --         },
-    --       },
-    --     })
-    --   end,
-    -- })
-
     vim.lsp.config("*", {
       capabilities = require("cmp_nvim_lsp").default_capabilities(),
     })
-    vim.lsp.enable("clangd")
-    vim.lsp.config.clangd = {
-      cmd = { "/home/ubuntu/tools/llvm-project/clangd_18.1.0/bin/clangd" }, -- Explicit path for ARM builds
-    }
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+
+    local uname = vim.loop.os_uname()
+    if uname.machine == "arm64" then
+      vim.lsp.config.clangd = {
+        -- Set explicit path for ARM builds. clangd does not distribute arm64 version so you need to build it yourself.
+        cmd = { "/home/ubuntu/tools/llvm-project/clangd_18.1.0/bin/clangd" },
+      }
+    end
   end,
 }
